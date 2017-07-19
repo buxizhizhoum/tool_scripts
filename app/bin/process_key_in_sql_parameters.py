d2bc17171c6d625be1a69147aca77afe48c0bda4
Think <think@think-ThinkPad-X260>
2017-07-19 11:13:21 +0800

    tool scripts used to modify sql

---
+++ app/bin/process_key_in_sql_parameters.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+This scripts is used to add "()", "%" and "s" in sql, which will be used in
+MySQLdb or pymysql, when execute sql with parameters. such as in the clauses 
+below:
+
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', 
+                            passwd='', db='tkq1')
+    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    cursor.execute(sql, args)
+
+the parameter sql in last clause should be something below if the 
+parameter args is an dict or an sequence of dict if execute_many(sql, args) 
+been used:
+
+        "INSERT INTO one_minutes_data (store_name, address)
+        VALUES (%(store_name)s, %(address)s)"
+    
+Generally, after we get the column we would like to insert values, such as 
+"store_name" and "address", however we have to edit the parts after "VALUES"
+manually, if there are too many columns, it would be boring and very easy
+to make mistakes.
+
+What this scripts does is to return a string that contains "%()s" in each 
+words of the columns been provided.
+for example, when "store_name, address" been provided, 
+"(%(store_name)s, %(address)s)" will be returned.
+"""
+
+
+class SQLprocessor(object):
+    def __init__(self):
+        pass
+
+    def wrap_sql(self, columns):
+        """
+        Accept a string contains all of the columns, wrap it with "%()s" 
+        and return
+        :param columns: a string contains all of the columns, which you 
+        would like to appear in sql eg: "store_name, address"
+        :return: a string whose words has been wrap with "%()s", 
+        eg: '%(store_name)s, %(address)s'
+        """
+        column_list = columns.split(",")
+        # remove space, add %()s on each item of list
+        tmp = ["%(" + item.strip(" ") + ")s" for item in column_list]
+        # join the list to from a string
+        result = ", ".join(tmp)
+        return result
+
+
+if __name__ == "__main__":
+    test_string = "store_name, address"
+    # test_string = "ua, ub, uc, uab, ucb, ia, ib, ic, costtl, cosa, cosb, " \
+    #               "cosc, pttl, pa, pb, pc"
+
+    sql_processor = SQLprocessor()
+    res = sql_processor.wrap_sql(test_string)
+    print res
