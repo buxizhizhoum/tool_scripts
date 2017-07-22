398f1e9541aa6ae1442a617f17f9dd2b081fad75
buxizhizhoum <mapeaks@126.com>
2017-07-22 10:25:40 +0800

    add

---
+++ app/bin/ticker_of_function.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+This module contains a decorator which is used to calculate the execute time
+of a function.
+
+When it is needed, just put @ticker before the definition of the function.
+"""
+import time
+import datetime
+
+
+def ticker(func):
+    # accept a function as parameter
+    def print_info(*args, **kwargs):
+        # do something before
+        print "-- Started '%s' on %s " % (
+            func.__name__,
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        )
+        start_time = time.time()
+        # run function
+        result = func(*args, **kwargs)
+        # do something after
+        end_time = time.time()
+        print "-- Finished '%s' on %s" % (
+            func.__name__,
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        )
+        print "Time consumed: %0.3f second(s)" % (end_time - start_time)
+        return result
+
+    print_info.__name__ = func.__name__
+    # return the result of print_info which is result of func.
+    return print_info
+
+
+if __name__ == "__main__":
+    @ticker
+    def add(x, y):
+        time.sleep(1)
+        return x + y
+
+
+    add(1, 2)
