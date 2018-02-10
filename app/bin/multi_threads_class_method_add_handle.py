6d0deac94aca9075469a3f4f4c976f998f042489
buxizhizhoum <mapeaks@126.com>
2018-02-10 11:51:50 +0800

    Add multi_threads_class_method_add_handle.py

---
+++ app/bin/multi_threads_class_method_add_handle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+after a method is decorated by decorator "make_multi_threads", the method will
+run in a thread if it is called.
+
+oppose that the requirement is to output 10 "wake up", two methods is
+available:
+
+    1. try_method:
+        this method runs in a threads, and saves no time
+    2. for loop plus try_method_simple, each loop will start a thread and will
+        save more time, if it is not a cpu heavy task.
+"""
+import time
+from threading import Thread
+
+
+def make_multi_threads(fn):
+    def thread_wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread  # return as a handle
+    return thread_wrapper
+
+
+class TryClass(object):
+    # this var will be used in function which will be call by threads
+    time_to_sleep = 1
+
+    @make_multi_threads
+    def try_method(self):
+        """
+        this not work as multi threads, this method runs in a thread, if multi
+        threads is needed, this method should be call multi times, each call
+        will start a thread.
+        :return:
+        """
+        for i in range(10):
+            time.sleep(self.time_to_sleep)
+            print("wake up")
+
+    @make_multi_threads
+    def try_method_simple(self):
+        """
+        better way to understand is to move for loop outside of the method, and
+        each loop will start a thread.
+        :return:
+        """
+        time.sleep(self.time_to_sleep)
+        print("wake up")
+
+    def multi_threads_try(self):
+        """
+        each loop will start a thread.
+        :return:
+        """
+        for i in range(10):
+            # each loop start a thread, and there will be 10 threads in total.
+            self.try_method_simple()  # this part will run in a thread.
+
+
+if __name__ == "__main__":
+    try_1 = TryClass()
+
+    # this will start 1 thread in each loop
+    for i in range(10):
+        handle = try_1.try_method_simple()
+        # handle.join()
